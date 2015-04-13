ea5a70b8fd288a588ed040b9980af904f6d23d27
JJ <indomitable7@naver.com>
2015-04-13 17:49:59 +0900

    ExistRect.java

---
+++ ExistRect.java
@@ -0,0 +1,27 @@
+package step5;
+import java.util.Scanner;
+
+public class ExistRect {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+
+		System.out.println("점(x,y)의 좌표를 입력하세요.");
+		int x = sc.nextInt();
+		int y = sc.nextInt();
+
+		if (x >= 50 && x <= 100) {
+			if (y >= 50 && y <= 100) {
+				System.out.println("점(" + x + "," + y
+						+ ")은 (50,50)과 (100,100)의 사각형 내에 있습니다.");
+			}else{
+				System.out.println("점(" + x + "," + y
+						+ ")은 (50,50)과 (100,100)의 사각형 내에 없습니다.");
+			}
+		}else{
+			System.out.println("점(" + x + "," + y
+					+ ")은 (50,50)과 (100,100)의 사각형 내에 없습니다.");
+		}
+	}
+
+}
